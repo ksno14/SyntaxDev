fc244dcb26d00be495d72dd6ee8c9305735ac692
ksno2 <sebastian_u14@outlook.com>
2023-08-13 18:51:32 -0500

    13/08

--- Main Page/index.js
+++ Main Page/index.js
@@ -1,9 +1,15 @@
-var colorMode = document.querySelector('#colorMode');
-colorMode.onclick=function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        colorMode.src="Img/moon.png";
-    }else{
-        colorMode.src="Img/sun.png";
-    }
-}
+var colorMode = document.querySelector("#colorMode");
+colorMode.onclick = function () {
+  if (localStorage.getItem("colormode")) {
+    console.log("owo");
+  } else {
+    localStorage.setItem("colormode",1)
+    console.log("unu")
+  }
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    colorMode.src = "Img/moon.png";
+  } else {
+    colorMode.src = "Img/sun.png";
+  }
+};
